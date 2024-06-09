d365a50ce7b618c8890c7b44e16e2c548f41cf38
Tulqinboyev-Ziyodulla <ziyodulla464@gmail.com>
2024-06-09 22:08:45 +0500

    js

---
+++ app.js
@@ -0,0 +1,136 @@
+alert("Hush kelibsiz")
+alert("Masalalar Console da")
+
+// {
+//     // 1- masala
+//     function perimetri (a) {
+//         return 4 * a;
+//     }
+    
+//     console.log(perimetri(5));
+// }
+
+
+// {
+//     // 2- masala
+//     function yuzasi(a) {
+//         return a * a;
+//     }
+    
+//     console.log(yuzasi(5));
+// }
+
+
+// {
+//     // 3- masala
+//     function yuzasi(a, b) {
+//         return a * b;
+//     }
+    
+//     function perimetri(a, b) {
+//         return 2 * (a + b);
+//     }
+
+//     console.log(yuzasi(5, 10));
+//     console.log(perimetri(5, 10));
+    
+// }
+
+
+// {
+//     // 4- masala
+//     function uzunligi(d) {
+//         const pi = 3.14;
+//         return pi * d;
+//     }
+    
+//     console.log(uzunligi(10));    
+// }
+
+
+// {
+//     // 5- masala
+//     function hajmi(a) {
+//         return a ** 3;
+//     }
+    
+//     function sirti(a) {
+//         return 6 * (a ** 2);
+//     }
+    
+//     console.log(hajmi(3));
+//     console.log(sirti(3));    
+// }
+
+
+// {
+//     // 6- masala
+//     function hajmi(a, b, c) {
+//         return a * b * c;
+//     }
+    
+//     function sirti(a, b, c) {
+//         return 2 * (a * b + b * c + a * c);
+//     }
+    
+//     console.log(hajmi(2, 3, 4)); 
+//     console.log(sirti(2, 3, 4));     
+// }
+
+
+// {
+//     // 7- masala
+//     function uzunligi(R) {
+//         const pi = 3.14;
+//         return 2 * pi * R;
+//     }
+    
+//     function yuzasi(R) {
+//         const pi = 3.14;
+//         return pi * (R ** 2);
+//     }
+    
+//     console.log(uzunligi(5));
+//     console.log(yuzasi(5));    
+// }
+
+
+// {
+//     // 8- masala
+//     function ortaArifmetigi(a, b) {
+//         return (a + b) / 2;
+//     }
+    
+//     console.log(ortaArifmetigi(10, 20));    
+// }
+
+
+// {
+//     // 9- masala
+//     function geometri(a, b) {
+//         return Math.sqrt(a * b);
+//     }
+    
+//     console.log(geometri(9, 16));    
+// }
+
+
+// {
+//     // 10- masala
+//     function son(a, b) {
+//         const yigindisi = a + b;
+//         const kopaytmasi = a * b;
+//         const aKvadrati = a ** 2;
+//         const bKvadrati = b ** 2;
+        
+//         return {
+//             yigindisi,
+//             kopaytmasi,
+//             aKvadrati,
+//             bKvadrati
+//         };
+//     }
+    
+//     const natija = son(3, 4);
+//     console.log(natija);    
+// }
